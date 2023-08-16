c5dd4ac870cf39c64b748b95146fbe61c788f67c
Wen Barbosa <wenblack012@gmail.com>
2023-08-16 10:57:14 -0300

    🎨 Import global styles for home Page 🎨

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,157 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Lato } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image'
+import LocationIcon from '@/assets/pin.svg'
+import WindIcon from '@/assets/temp-wind.svg'
+import HumidityIcon from '@/assets/temp-humidity.svg'
+import RainIcon from  '@/assets/weather-rain.svg'
+import LeafIcon from '@/assets/leaf.svg'
+import SunTime from '@/assets/sun-time.svg'
+import SunChart from '@/assets/sun-chart.svg'
+import CloudsIcon from '@/assets/weather-clouds.svg'
+import SunIcon from '@/assets/weather-sun.svg'
+import ThunderIcon from '@/assets/weather-thunder.svg'
+import CloudyIcon from '@/assets/weather-cloudy.svg'
+import Rain from '@/assets/temp-rain.svg'
+
+const lato = Lato({ subsets: ['latin'] , weight:['400','700']})
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={`${lato.className}`}>
+      <section className="temperature-now">
+        <div className="location">
+          <Image 
+            src={LocationIcon} 
+            alt="icone de localizacao" 
+          />
+          <strong>Rio do Sul, SC</strong>
         </div>
-      </div>
+        <div className="temp">
+          <div className="number">
+            18
+            <div className="maxmin">22° <span>16° </span></div>
+          </div>
+          <div className="celsius">°C</div>
+        </div>
+        <div className="statistics">
+          <div className="stats">
+            <Image src={WindIcon} alt="icone de vento" />
+            <div className="info">
+              <p>Vento</p>
+              <h5>17 <span>km/h</span></h5>
+            </div>
+          </div>
+          <div className="stats">
+            <Image src={HumidityIcon} alt="icone de umidade" />
+            <div className="info">
+              <p>Umidade</p>
+              <h5>31 <span>%</span></h5>
+            </div>
+          </div>
+          <div className="stats">
+            <Image src={Rain} alt="icone de chuva" />
+            <div className="info">
+              <p>Chuva</p>
+              <h5>10 <span>%</span></h5>
+            </div>
+          </div>
+        </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="air-quality">
+        <h2 className="title">
+          <Image src={LeafIcon} alt="icone de folha de árvore" />
+          Qualidade do ar
+        </h2>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <p className="good">Boa</p>
+        <p className="number">21</p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="info">
+          <div className="number">
+            <p>12.9</p>
+            <small>PM2.5</small>
+          </div>
+          <div className="number">
+            <p>12.9</p>
+            <small>PM10</small>
+          </div>
+          <div className="number">
+            <p>2.1</p>
+            <small>SO₂</small>
+          </div>
+          <div className="number">
+            <p>1.4</p>
+            <small>NO₂</small>
+          </div>
+          <div className="number">
+            <p>21.2</p>
+            <small>O₃</small>
+          </div>
+          <div className="number">
+            <p>0.7</p>
+            <small>CO</small>
+          </div>
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+      <section className="sun-time">
+        <h2 className="title">
+          <Image
+            src={SunTime}
+            alt="icone de um sol com um relógio dentro"
+          />
+          Horário do sol
+        </h2>
+        <div className="sun-chart-wrapper">
+          <div className="sun-chart">
+            <div className="chart">
+              <Image
+                src={SunChart}
+                alt="imagem de um gráfico semi circulo com traços"
+              />
+            </div>
+            <time className="now">17:48</time>
+          </div>
+        </div>
+        <div className="time">
+          <time className="sunrise">06:00</time>
+          <time className="sunset">18:52</time>
+        </div>
+      </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="week-weather">
+        <div className="day">
+          <h4 className="title">Amanhã</h4>
+          <Image src={CloudsIcon} alt="" />
+          <p className="maxmin">21° <span>16°</span></p>
+        </div>
+
+        <div className="day">
+          <h4 className="title">Sexta</h4>
+          <Image src={SunIcon} alt="" />
+          <p className="maxmin">28° <span>16°</span></p>
+        </div>
+
+        <div className="day">
+          <h4 className="title">Sábado</h4>
+          <Image src={RainIcon} alt="" />
+          <p className="maxmin">20° <span>16°</span></p>
+        </div>
+
+        <div className="day">
+          <h4 className="title">Domingo</h4>
+          <Image src={ThunderIcon} alt="" />
+          <p className="maxmin">28° <span>26°</span></p>
+        </div>
+
+        <div className="day">
+          <h4 className="title">Segunda</h4>
+          <Image src={CloudyIcon} alt="" />
+          <p className="maxmin">26° <span>20°</span></p>
+        </div>
+      </section>
     </main>
   )
 }
